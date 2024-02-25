a4eaf36c80c29daf20b06ba1434df1ceb65d28b0
Rnh81 <rnh-1981@mail.ru>
2024-02-25 22:12:33 +0400

    KS-1-2 print data from first lesson

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+    //println("Это решение задачи 1 из урока 1")
+
+    val numberSneakrs = 75
+    val notificationText = "Благодарим за покупку!"
+    //println(numberSneakrs)
+    //println(notificationText)
+
+    val numberOfStaff = 2000
+
+    //println(numberOfStaff)
+
+    val departed = 1
+    val sum = numberOfStaff - departed
+    println(sum)
+}
